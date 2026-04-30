174764a90abafaffb88f2f475f213a427d24e70e
mandarwagh9 <mandarwagh90@gmail.com>
2026-04-30 23:38:15 +0530

    feat(frontend): add ErrorBoundary so component errors don't blank the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AdminDashboard from './pages/AdminDashboard';
 import MobileCamera from './pages/MobileCamera';
+import ErrorBoundary from './components/ErrorBoundary';
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/mobile" element={<MobileCamera />} />
-      <Route path="/*" element={<AdminDashboard />} />
-    </Routes>
+    <ErrorBoundary>
+      <Routes>
+        <Route path="/mobile" element={<MobileCamera />} />
+        <Route path="/*" element={<AdminDashboard />} />
+      </Routes>
+    </ErrorBoundary>
   );
 }
 
